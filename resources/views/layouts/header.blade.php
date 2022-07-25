554da9bd72adfd286b06caef0fad35c513b96a6d
Azisten <azistencom@gmail.com>
2022-07-25 10:38:05 +0700

    Fix Navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,6 +5,10 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="/js/app.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/init-alpine.js') }}"></script>
+<script src="{{ asset('js/trix.js') }}"></script>
+
+{{--
 <script src="/js/init-alpine.js"></script>
-<script src="/js/trix.js"></script>
+<script src="/js/trix.js"></script> --}}
