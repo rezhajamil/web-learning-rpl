120d5cbefcf8965c0e1210dc1c341d5cf4b05472
Azisten <azistencom@gmail.com>
2022-08-08 20:56:34 +0700

    Revisi

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -11,34 +11,34 @@
     </div>
     <button class="fixed px-4 py-2 font-bold text-purple-900 transition-all bg-white rounded-lg hover:bg-gray-100 bottom-6 right-6" x-on:click="profile=true"><i class="mr-4 text-lg bi bi-person-square"></i>Profil Pengembang</button>
 
-    <div class="fixed inset-0 z-30 flex items-center justify-center w-full h-full bg-black/80" x-transition x-show="profile">
-        <div class="flex flex-col w-1/2 px-8 py-4 bg-white rounded-lg">
+    <div class="fixed inset-0 z-30 flex items-center justify-center w-full h-full p-6 bg-black/80" x-transition x-show="profile">
+        <div class="flex flex-col w-full px-8 py-4 bg-white rounded-lg md:w-1/2">
             <button class="mb-4 ml-auto text-3xl font-bold transition-all hover:text-purple-800" x-on:click="profile=false">X</button>
             <div class="mx-auto overflow-hidden rounded-md h-52 w-36">
                 <img src="{{ asset('images/kartini.jpg') }}" alt="profil" class="w-full h-full">
             </div>
             <div class="grid grid-cols-2 gap-1 my-6">
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">Nama</span>
                     <span class="block font-base">Kartini Juita Nainggolan</span>
                 </div>
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">NIM</span>
                     <span class="block font-base">5181151005</span>
                 </div>
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">Tempat Tanggal Lahir</span>
                     <span class="block font-base">Indrapura, 21 April 2000</span>
                 </div>
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">Email</span>
                     <span class="block font-base">kartininainggolan91@gmail.com</span>
                 </div>
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">Prodi</span>
                     <span class="block font-base">Pendidikan Teknologi Informatika dan Komputer / Jurusan Pendidikan Teknik Elektro / Fakultas Teknik/ Unimed</span>
                 </div>
-                <div class="px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md md:col-span-1">
                     <span class="block font-bold underline">Riwayat Pendidikan</span>
                     <ol>
                         <li class="font-base">1. SD Negeri 017976 Indrapura</li>
@@ -46,7 +46,7 @@
                         <li class="font-base">3. SMA Negeri 1 Air Putih</li>
                     </ol>
                 </div>
-                <div class="col-span-2 px-3 py-2 rounded shadow-md">
+                <div class="col-span-2 px-3 py-2 rounded shadow-md ">
                     <span class="block font-bold underline">Ringkasan Media</span>
                     <span class="block font-base">Media ini dikembangkan sebagai media penulis dalam melakukan penelitian di SMK Sinar Husni Medan. Media ini diharapkan mampu membantu siswa dalam kegiatan belajar mengajar agar tujuan pembelajaran dapat terwujud.</span>
 
